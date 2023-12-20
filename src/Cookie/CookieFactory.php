cf2adad0ceb3bb8309414108c2adf0a0909ce926
Quentin Brouillet <quentin.brouillet@elao.com>
2023-12-20 14:39:02 +0100

    Cookie Factory - Json validation and log if null

--- src/Cookie/CookieFactory.php
+++ src/Cookie/CookieFactory.php
@@ -10,6 +10,7 @@
 
 namespace Elao\Bundle\ConsentBundle\Cookie;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -17,11 +18,13 @@ class CookieFactory
 {
     private string $name;
     private int $ttl;
+    private LoggerInterface $logger;
 
-    public function __construct(string $name, int $ttl)
+    public function __construct(string $name, int $ttl, LoggerInterface $logger)
     {
         $this->name = $name;
         $this->ttl = $ttl;
+        $this->logger = $logger;
     }
 
     public function create(array $consents): Cookie
@@ -34,6 +37,16 @@ class CookieFactory
 
     public function read(Request $request): array
     {
-        return json_decode($request->cookies->get($this->name, '[]'), true);
+        $cookieValue = json_decode($request->cookies->get($this->name, '[]'), true);
+
+        if (null === $cookieValue) {
+            $this->logger->warning('Consent Bundle - Decoded cookie value return null', [
+                'Cookie value' => $request->cookies->get($this->name),
+            ]);
+
+            return [];
+        }
+
+        return $cookieValue;
     }
 }
